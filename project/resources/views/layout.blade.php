a12b6d3f1bf21f5bcc4a65328dd7a3672d55384f
Kristaps Jaremčuks <kristaps.jaremcuks@cloudcredit.net>
2020-02-28 23:19:00 +0200

    cleanup

--- project/resources/views/layout.blade.php
+++ project/resources/views/layout.blade.php
@@ -7,7 +7,7 @@
     <title>Euro Exchange Rates</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sticky-footer/">
+
     <style>
         h1 > a:hover {text-decoration: none;}
 
